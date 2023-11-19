ca3eae36b91ff925f5d8dc8ff13a1bb6e925d767
Razen <70081850+Razen21@users.noreply.github.com>
2023-11-18 23:03:01 -0500

    Created IClientSocket.cs interface. Created ClientVersion record. Created ClientLocale enum.

---
+++ src/Common/Eternal.Network/Socket/IClientSocket.cs
@@ -0,0 +1,40 @@
+﻿using Eternal.Network.Messaging;
+
+namespace Eternal.Network.Socket
+{
+    /// <summary>
+    /// Interface representing a network client socket.
+    /// </summary>
+    public interface IClientSocket
+    {
+        /// <summary>
+        /// Immutable client version information used in session verification and encryption.
+        /// </summary>
+        ClientVersionRecord Version { get; set; }
+
+        /// <summary>
+        /// Randomly generated <see cref="uint"/> value used for session verification and encryption.
+        /// </summary>
+        uint RecieveSequence { get; set; }
+
+        /// <summary>
+        /// Randomly generated <see cref="uint"/> value used for session verification and encryption.
+        /// </summary>
+        uint SendSequence { get; set; }
+
+        /// <summary>
+        /// Asynchronously sends an <see cref="IPacket"/> to the client associated with this socket.
+        /// </summary>
+        /// <param name="packet">The packet to send.</param>
+        /// <param name="cancellation">Optional <see cref="CancellationToken"/> used to cancel the send operation.</param>
+        /// <returns>A <see cref="Task"/> representing the send packet operation.</returns>
+        Task SendPacketAsync(IPacket packet, CancellationToken cancellation = default);
+
+        /// <summary>
+        /// Asynchronously closes the socket.
+        /// </summary>
+        /// <param name="cancellation">Optional <see cref="CancellationToken"/> used to cancel the close operation.</param>
+        /// <returns>A <see cref="Task"/> representing the close operation.</returns>
+        Task CloseAsync(CancellationToken cancellation = default);
+    }
+}
