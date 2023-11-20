5bf63fe61596ddd9baa648667a834c3d838f2e32
Razen <70081850+Razen21@users.noreply.github.com>
2023-11-20 10:07:05 -0500

    Created CryptographyOperations.cs static class. Created Aes, IG, and Triple Des Cipher classes

---
+++ src/Common/Eternal.Cryptography/Ciphers/TripleDesCipher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eternal.Cryptography.Ciphers
+{
+    internal static class TripleDesCipher
+    {
+        private static readonly Lazy<TripleDES> LazyTripleDesInstance = new Lazy<TripleDES>(() =>
+        {
+            var cipher = TripleDES.Create();
+
+            cipher.GenerateIV();
+            cipher.Mode = CipherMode.ECB;
+            cipher.BlockSize = 64;
+            cipher.Padding = PaddingMode.None;
+            cipher.FeedbackSize = 64;
+        });
+
+
+        internal static string Decrypt(byte[] input, byte[] key)
+        {
+            LazyTripleDesInstance.Value.Key = key;
+
+            using var memoryStream = new MemoryStream(input);
+            using var decryptor = LazyTripleDesInstance.Value.CreateDecryptor();
+            using var cryptoSteam = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            using var streamReader = new StreamReader(cryptoSteam);
+
+            return streamReader.ReadToEnd();
+        }
+    }
+}
